c0dbd2721cc723c1f4369560a3f03b74b2df3c63
Fangming Cheng <chengfangming1993@gmail.com>
2023-05-12 19:48:33 +0800

    添加Callback回调

--- pure/models/HelloApi/java/com/pure/api/HelloManager.java
+++ pure/models/HelloApi/java/com/pure/api/HelloManager.java
@@ -4,6 +4,7 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.pure.IHelloService;
 import android.util.Log;
+import android.pure.ICallback;
 
 public class HelloManager {
     private static final String TAG = "HelloManager";
@@ -25,6 +26,24 @@ public class HelloManager {
         try {
             Log.d(TAG, "sayHello " + name);
             mService.hello(name);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void registerCallback(int pid, ICallback callback) {
+        try {
+            Log.d(TAG, "registerCallback");
+            mService.registerCallback(pid, callback);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void unregisterCallback(int pid) {
+        try {
+            Log.d(TAG, "unregisterCallback");
+            mService.unregisterCallback(pid);
         } catch (RemoteException e) {
             e.printStackTrace();
         }
--- pure/models/PureSettings/java/com/cfm880/puresettings/MainActivity.java
+++ pure/models/PureSettings/java/com/cfm880/puresettings/MainActivity.java
@@ -1,14 +1,23 @@
 package com.cfm880.puresettings;
 
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import com.pure.api.HelloManager;
+import android.pure.ICallback;
 
 import android.app.Activity;
 
 public class MainActivity extends Activity {
     private final static String TAG = "PureSettings";
+    private ICallback callback = new ICallback.Stub() {
+
+        @Override
+        public void onMessage(String message) throws RemoteException {
+            Log.d(TAG, "onMessage:" + message);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +29,18 @@ public class MainActivity extends Activity {
             }
         });
         HelloManager.getInstance();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        HelloManager.getInstance().registerCallback(android.os.Process.myPid(), callback);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        HelloManager.getInstance().unregisterCallback(android.os.Process.myPid());
     }
 
     private void test() {
