e3529ec34fef2932fe8b2c4e03a8d33c415203e6
FangmingCheng <chengfangming1993@gmail.com>
2023-05-31 19:55:25 +0800

    再次拆分sepolicy

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -2,3 +2,4 @@ PRODUCT_PUBLIC_SEPOLICY_DIRS += device/cfm880/sepolicy/public
 PRODUCT_PRIVATE_SEPOLICY_DIRS += device/cfm880/sepolicy/private
 BOARD_SEPOLICY_DIRS += device/cfm880/sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += device/cfm880/pure/models/hidl/tvserver/1.0/default/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/cfm880/pure/models/HelloNativeService/sepolicy
