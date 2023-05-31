181d8217fe61e292e782f42090a0c7601778403b
FangmingCheng <chengfangming1993@gmail.com>
2023-05-31 20:02:26 +0800

    java层 sepolicy

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -3,3 +3,4 @@ PRODUCT_PRIVATE_SEPOLICY_DIRS += device/cfm880/sepolicy/private
 BOARD_SEPOLICY_DIRS += device/cfm880/sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += device/cfm880/pure/models/hidl/tvserver/1.0/default/sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += device/cfm880/pure/models/HelloNativeService/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/cfm880/pure/models/HelloApi/sepolicy
